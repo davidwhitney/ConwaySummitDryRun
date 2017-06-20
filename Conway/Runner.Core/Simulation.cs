5b2beb146df93992496abae21a6bc8e4cdb51f63
David Whitney <david.whitney@thetrainline.com>
2017-06-20 13:08:31 +0100

    Initial state.

---
+++ Conway/Runner.Core/Simulation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runner.Core
+{
+    public class Simulation
+    {
+        public void Step()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Conway/Runner/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using Runner.Core;
+
+namespace Runner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sim = new Simulation();
+            while (true)
+            {
+                sim.Step();
+                var state = sim.ToString();
+
+                Console.Clear();
+                Console.WriteLine(state);
+
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
